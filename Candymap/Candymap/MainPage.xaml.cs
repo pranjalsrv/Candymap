6d934231fffb690330dcf894e5c9aecc72c58465
Pranjal Srivastava <Pranjal.Srivastava@trigyn.com>
2019-06-11 16:14:56 +0530

    single line path done

--- Candymap/Candymap/MainPage.xaml.cs
+++ Candymap/Candymap/MainPage.xaml.cs
@@ -10,13 +10,13 @@ using System.Reflection;
 
 namespace Candymap
 {
-    
+
     public partial class MainPage : ContentPage
     {
-        
+
         public MainPage()
         {
-            
+
             InitializeComponent();
 
             int numButtonsInPattern = 6;
@@ -25,9 +25,8 @@ namespace Candymap
             Color buttonColor = Color.Default;
             int buttonWidth = 60;
             int buttonHeight = 60;
-            int randomnessFactor = 50;
+            //int randomnessFactor = 50;
             var curlinessFactor = 60;
-            double currentYLocation = 0.0;
 
 
             int scrollThreshold = heightScale * numButtonsInPattern / 4;
@@ -36,10 +35,7 @@ namespace Candymap
 
 
             var buttons = new List<Button>();
-            var skPointsListc = new List<SKPoint[]>();
-            
-            //var embeddedImage = new Image { Source = ImageSource.FromResource("Candymap.Images.BGoverlayf.png", typeof(MainPage).GetTypeInfo().Assembly),TranslationY = 25 };
-            //ImageGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            var skLineList = new List<SKPoint[]>();
             Random rnd = new Random();
             Size size = Device.Info.PixelScreenSize;
             double screenWidth = size.Width;
@@ -49,15 +45,16 @@ namespace Candymap
             var firstPoints = new List<SKPoint>();
 
 
+
             void buttonCreater(int iteration)
             {
-                
+
                 var xyPoint = new List<SKPoint>();
 
 
                 for (var i = 0; i < numButtonsInPattern; i++)
                 {
-                    
+
                     int indexForButtonText = buttons.Count - 1;
                     //if (indexForButtonText == 241) { color = Color.Green; }
                     //else { color = Color.Default; }
@@ -73,21 +70,20 @@ namespace Candymap
                     int currentButtonIndex = buttons.Count - 1;
                     int placementIndex = currentButtonIndex;
 
-                    while (placementIndex > numButtonsInPattern-1) {
+                    while (placementIndex > numButtonsInPattern - 1) {
                         placementIndex = placementIndex - numButtonsInPattern;
                     }
 
                     if (i < numButtonsInPattern / 2.0f)
                     {
                         var x = 0 + placementIndex * (2.0 / (numButtonsInPattern));
-                        var y = currentButtonIndex * heightScale + Math.Sin((i / (numButtonsInPattern / 8.0f)) * Math.PI / 2) * curlinessFactor ;
+                        var y = currentButtonIndex * heightScale + Math.Sin((i / (numButtonsInPattern / 8.0f)) * Math.PI / 2) * curlinessFactor;
                         //var y = currentButtonIndex * heightScale + rnd.Next(-randomnessFactor, +randomnessFactor/2);
                         AbsoluteLayout.SetLayoutBounds(buttons[currentButtonIndex], new Rectangle(x, y, buttonWidth, buttonHeight));
                         AbsoluteLayout.SetLayoutFlags(buttons[currentButtonIndex], AbsoluteLayoutFlags.XProportional);
 
-                        if (i == 0) { xyPoint.Add(new SKPoint((float)(x * widgetWidth) +30, (float)y + 30)); }
+                        if (i == 0) { xyPoint.Add(new SKPoint((float)(x * widgetWidth) + 30, (float)y + 30)); }
                         else { xyPoint.Add(new SKPoint((float)(x * widgetWidth), (float)y + 30)); }
-                        //Console.WriteLine("x=" + ((float)(x * widgetWidth)).ToString() + "y=" + y.ToString());
                     }
                     else if (i == numButtonsInPattern / 2.0f)
                     {
@@ -96,11 +92,9 @@ namespace Candymap
                         //var y = currentButtonIndex * heightScale + rnd.Next(-randomnessFactor/2, +randomnessFactor);
                         AbsoluteLayout.SetLayoutBounds(buttons[currentButtonIndex], new Rectangle(x, y, buttonWidth, buttonHeight));
                         AbsoluteLayout.SetLayoutFlags(buttons[currentButtonIndex], AbsoluteLayoutFlags.XProportional);
-
-                        //Console.WriteLine("x=" + ((float)(x * widgetWidth)).ToString() + "y=" + y.ToString()); 
-                        xyPoint.Add(new SKPoint(((float)x * widgetWidth)-30, y+30));
+                        xyPoint.Add(new SKPoint(((float)x * widgetWidth) - 30, y + 30));
                     }
-                    
+
                     else
                     {
                         var x = 2 - placementIndex * (2.0 / (numButtonsInPattern));
@@ -109,33 +103,26 @@ namespace Candymap
                         AbsoluteLayout.SetLayoutBounds(buttons[currentButtonIndex], new Rectangle(x, y, buttonWidth, buttonHeight));
                         AbsoluteLayout.SetLayoutFlags(buttons[currentButtonIndex], AbsoluteLayoutFlags.XProportional);
 
-                        xyPoint.Add(new SKPoint((float)(x * widgetWidth), (float)y+30));
-
-                        //Console.WriteLine("x=" + ((float)(x * widgetWidth)).ToString() + "y=" + y.ToString());
+                        xyPoint.Add(new SKPoint((float)(x * widgetWidth), (float)y + 30));
                     }
                     MainAbsoluteLayout.Children.Add(buttons[currentButtonIndex]);
-                    
                 }
 
                 firstPoints.Add(xyPoint[0]);
                 lastPoints.Add(xyPoint[numButtonsInPattern - 1]);
 
-                for (var j = 0; j < numButtonsInPattern-1; j++)
+                for (var j = 0; j < numButtonsInPattern - 1; j++)
                 {
-                    skPointsListc.Add(new SKPoint[]
+                    skLineList.Add(new SKPoint[]
                     {
                             xyPoint[j],xyPoint[j+1]
                     });
                 }
-                //ImageGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(iteration, GridUnitType.Star) });
-                //ImageStack.Children.Add(embeddedImage);
-                
             }
-            
 
-            void Draw_RandomShape(SKCanvas skCanvas)
+
+            void Draw_RandomShape(SKCanvas skCanvas, int curveNumber)
             {
-                // Draw any kind of Shape
                 SKPaint strokePaint = new SKPaint
                 {
                     Style = SKPaintStyle.Stroke,
@@ -144,25 +131,147 @@ namespace Candymap
                     IsAntialias = true,
                     StrokeCap = SKStrokeCap.Round,
                 };
-                
-                
-                //Console.WriteLine("heyheyhey" + skPointsListc.Count);
-                foreach (var elem in skPointsListc) { 
-                    //skCanvas.DrawLine(elem[0].X, elem[0].Y, elem[1].X, elem[1].Y, strokePaint);
-                    using (SKPath path = new SKPath())
+                try
+                {
+                    for (var i = 0; i < lastPoints.Count; i++)
                     {
-                        //SKPoint center = new SKPoint(10+(elem[0].X + elem[1].X)/2.0f,10+(elem[0].Y + elem[1].Y) /2.0f);
-                        path.MoveTo(elem[0]);
-                        //ArcTo (Single rx, Single ry, Single xAxisRotate, SKPathArcSize largeArc, SKPathDirection sweep, Single x, Single y)
-                        path.ArcTo(new SKPoint(180,180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
-                        path.MoveTo(elem[0]);
-                        path.ArcTo(new SKPoint(180,180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
-                        //path.ArcTo(elem[0], elem[1], 10);
-                        skCanvas.DrawPath(path, strokePaint);
+                        /*skLineList.Add(new SKPoint[] {
+                            lastPoints[i],firstPoints[i+1]
+                        });*/
+
+                        using (SKPath path = new SKPath())
+                        {
+                            path.MoveTo(lastPoints[i]);
+                            path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, firstPoints[i + 1]);
+                            path.MoveTo(lastPoints[i]);
+                            path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, firstPoints[i + 1]);
+                            skCanvas.DrawPath(path, strokePaint);
+                        }
                         
                     }
                 }
+                catch { }
+
+                skLineList = skLineList.Distinct().ToList();
+                bool firstDone = false;
+                bool secondDone = false;
+                var donePoints = new List<SKPoint[]>();
+                bool nextis3 = false;
+
+                foreach (var elem in skLineList) {
+                    if (!donePoints.Contains(elem))
+                    {
+                        //skCanvas.DrawLine(elem[0].X, elem[0].Y, elem[1].X, elem[1].Y, strokePaint);
+                        using (SKPath path = new SKPath())
+                        {
+                            if ((elem[1].Y - elem[0].Y) > 0 && (elem[1].X - elem[0].X) > 0 && !firstDone)
+                            {  //0 -> 1
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+
+                                firstDone = true;
+                            }
+                            else if ((elem[1].Y - elem[0].Y) > 0 && (elem[1].X - elem[0].X) > 0 && firstDone)
+                            {  //2 -> 3
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+
+                                firstDone = false;
+                            }
+                            if ((elem[1].Y - elem[0].Y) < 0 && (elem[1].X - elem[0].X) > 0)
+                            {  //1 -> 2
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                            }
+                             
+                             if ((elem[1].Y - elem[0].Y) > 0 && (elem[1].X - elem[0].X) < 0)
+                            {  //3 -> 4
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                
+                            }
+                            if ((elem[1].Y - elem[0].Y) < 0 && (elem[1].X - elem[0].X) < 0)
+                            {  //4 -> 5
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                path.MoveTo(elem[0]);
+                                path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                            }
+                             
+
+
+                            /*if (!donePoints.Contains(elem))
+                            {
+                                if (curveNumber == 0)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                    curveNumber++;
+                                }
+                                else if (curveNumber == 1)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);
+                                    curveNumber++;
+                                }
+                                else if (curveNumber == 2)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    curveNumber++;
+                                }
+                                else if (curveNumber == 3)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    curveNumber++;
+                                }
+                                else if (curveNumber == 4)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    curveNumber++;
+                                }
+                                else if (curveNumber == 5)
+                                {
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    path.MoveTo(elem[0]);
+                                    path.ArcTo(new SKPoint(180, 180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                                    curveNumber = 0;
+                                }*/
+
+                            donePoints.Add(elem);
+                            //SKPoint center = new SKPoint(10+(elem[0].X + elem[1].X)/2.0f,10+(elem[0].Y + elem[1].Y) /2.0f);
+
+                            //ArcTo (Single rx, Single ry, Single xAxisRotate, SKPathArcSize largeArc, SKPathDirection sweep, Single x, Single y)
+                            /*path.ArcTo(new SKPoint(180,180), 45, SKPathArcSize.Small, SKPathDirection.Clockwise, elem[1]);
+                            path.MoveTo(elem[0]);
+                            path.ArcTo(new SKPoint(180,180), 45, SKPathArcSize.Small, SKPathDirection.CounterClockwise, elem[1]);*/
+                            skCanvas.DrawPath(path, strokePaint);
+                        }
+                }
             }
+        }
+    
 
             void SkCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
             {
@@ -173,25 +282,13 @@ namespace Candymap
                 skCanvas.Clear(SKColors.Transparent);
                 var skCanvasWidth = info.Width;
                 var skCanvasheight = info.Height;
-
-                try
-                {
-                    for (var i = 0; i < lastPoints.Count; i++)
-                    {
-                        skPointsListc.Add(new SKPoint[] {
-                            lastPoints[i],firstPoints[i+1]
-                        });
-                    }
-                }
-                catch { }
                 
-                skCanvas.Scale(skCanvasWidth/(float)widgetWidth); 
-                // set the pixel scale of the canvas
-                Draw_RandomShape(skCanvas);
+                skCanvas.Scale(skCanvasWidth/(float)widgetWidth);
+                int curveNumber = 0;
+                Draw_RandomShape(skCanvas, curveNumber);
             }
-
-            //Console.WriteLine("heyheyhey"+screenHeight.ToString());
-            for (var i = 0; i < 3; i++) { buttonCreater(i); TotalIterations++; }
+            
+            for (var i = 0; i < 4; i++) { buttonCreater(i); TotalIterations++; }
             //while(MainScrollView.ContentSize.Height < screenHeight) { buttonCreater(TotalIterations); TotalIterations++; }
             
             SkCanvasView.PaintSurface += SkCanvasView_OnPaintSurface;
@@ -206,13 +303,13 @@ namespace Candymap
             }*/
 
             MainScrollView.Scrolled+= (object sender, ScrolledEventArgs e) => {
-                if (e.ScrollY > MainScrollView.ContentSize.Height - MainScrollView.Height+1) {
-                    //Console.WriteLine("heyheyhey"+MainScrollView.ContentSize.Height);
+                if (e.ScrollY > MainScrollView.ContentSize.Height - MainScrollView.Height -30) {
                     buttonCreater(TotalIterations);
                     TotalIterations++;
-                    Device.BeginInvokeOnMainThread(() => {
+                    //Device.BeginInvokeOnMainThread(() =>
+                    //{
                         SkCanvasView.InvalidateSurface();
-                    });
+                    //});
                 }
             };
         }
